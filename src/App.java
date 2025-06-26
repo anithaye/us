b2c5f8da496fed6c700a97614830e489ca4b49ff
anita <nitacrb58@gmail.com>
2025-06-26 11:49:48 +0700

    app

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Anita!");
+        System.out.println("Hello, Anita and Maya!");
     }
 }
